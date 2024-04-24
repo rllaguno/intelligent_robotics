ce53b2719b9c1f5868fcd917d603ec57214b4aa9
angieipaez <a01721772@tec.mx>
2024-04-23 21:07:42 -0600

    pid distancia y angulo

--- manchester_robotics/src/mini_challenge_2/mini_challenge_2/Controller.py
+++ manchester_robotics/src/mini_challenge_2/mini_challenge_2/Controller.py
@@ -7,8 +7,8 @@ import yaml
 import math 
 
 class My_Publisher(Node):
-    def _init_(self):
-        super()._init_('Controller')
+    def __init__(self):
+        super().__init__('Controller')
         self.odom = self.create_publisher(Twist, '/cmd_vel', 10)
         
         qos_profile = rclpy.qos.qos_profile_sensor_data
@@ -17,11 +17,22 @@ class My_Publisher(Node):
         
         self.timer_period_controller = 0.1
         self.timer_controller = self.create_timer(self.timer_period_controller, self.timer_callback_controller)
+        #self.timer_PID = self.create_timer(self.timer_period_controller, self.timer_callback_PID)
         self.get_logger().info('|Controller node successfully initialized|')
         
         self.msg_vel = Twist()
         self.msg_pose = Pose2D()
         self.msg_point = Point()
+
+        ### Distance ###
+        self.kpDistance = 5.2536
+        self.kiDistance = 0.1922
+        self.kdDistance = 4.9357
+
+        ### Angle ###
+        self.kpAngle = 0.4535
+        self.kiAngle = 0.0531
+        self.kdAngle = 0.3597
     
     def timer_callback_controller(self):
 
@@ -49,18 +60,41 @@ class My_Publisher(Node):
             angleRadians = math.atan(opuesto/adyacente)
             angleTarget = math.degrees(angleRadians) + 180
 
+        self.errorAngle =  angleTarget - self.msg_pose.theta 
 
-        self.errorTheta =  angleTarget - self.msg_pose.theta 
-
-        if (self.errorTheta > 180):
-            self.errorTheta = self.errorTheta - 360 
+        if (self.errorAngle > 180):
+            self.errorAngle = self.errorAngle - 360 
             
         self.errorDistance = math.hypot((abs(self.msg_pose.x)  - abs(self.msg_point.x)), (abs(self.msg_pose.y)  - abs(self.msg_point.y)))
 
 
+        ### PID DISTANCE ###
+        self.proportionalDistance = self.errorDistance
+        self.integralDistance = self.integralDistance + (self.errorDistance * 0.1)
+        self.derivativeDistance = (self.errorDistance - self.previousErrorDistance) / 0.1
+        self.previousErrorDistance = self.errorDistance
+        self.pidDistance = (self.kpDistance * self.proportionalDistance) + (self.kiDistance * self.integralDistance) + (self.kdDistance * self.derivativeDistance)
+        
+        if (self.pidDistance > 0.3) :
+            self.msg_vel.linear.x = 0.3
+            self.vel.publish(self.msg_vel) 
+        else :
+            self.msg_vel.linear.x = self.pidDistance
+            self.vel.publish(self.msg_vel) 
 
-        self.msg_vel.linear.x = 0.0
-        self.msg_vel.angular.z = 0.0
+        ### PID ANGLE ###
+        self.proportionalAngle = self.errorAngle
+        self.integralAngle = self.integralAngle + (self.errorAngle * 0.1)
+        self.derivativeAngle = (self.errorAngle - self.previousErrorAngle) / 0.1
+        self.previousErrorDAngle = self.errorAngle
+        self.pidAnglee = (self.kpAngle * self.proportionalAngle) + (self.kiAngle * self.integralAngle) + (self.Angle * self.derivativeAngle)
+        
+        if (self.pidAngle > 0.3) :
+            self.msg_vel.angular.z = 0.3
+            self.vel.publish(self.msg_vel) 
+        else :
+            self.msg_vel.angular.z = self.pidDistance
+            self.vel.publish(self.msg_vel) 
 
     def timer_callback_odometry(self, msg):
         self.msg_pose.x = msg.x
@@ -79,5 +113,5 @@ def main(args=None):
     m_p.destroy_node()
     rclpy.shutdown()
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
